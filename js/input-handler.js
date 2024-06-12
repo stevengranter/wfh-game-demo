b9f686bf6e51fb3ec91b7df7c4d36869e744da64
Steven Granter <stevengranter@gmail.com>
2024-06-12 11:49:33 -0230

    Clean-up and refactor of InputHandler class 🧼

--- js/input-handler.js
+++ js/input-handler.js
@@ -1,179 +1,114 @@
-"use strict"
+"use strict";
 
 export default class InputHandler {
 
     constructor(ui) {
 
-        this.ui = ui
-        this.right = false
-        this.left = false
-        this.up = false
-        this.down = false
+        // Hold a reference to UI instance passed to constructor
+        this.ui = ui;
 
-        this.escape = false
+        // List of action verbs that are available to the player,
+        // all input handlers convert all valid input to one of these 
+        // action verbs
+        this.right = false;
+        this.left = false;
+        this.up = false;
+        this.down = false;
+        this.pause = false;
+        this.escape = false;
 
-
-
-        addEventListener("keydown", (e) => {
-            e.preventDefault()
-            this.listenForKeys(e)
-        })
-        addEventListener("keyup", (e) => this.listenForKeys(e))
-
-
-        const touchOverlay = document.getElementById("touch-controller-overlay")
-        const virtualController = document.getElementById("ui--virtual-controller")
-
-        // prevent context menu from appearing on pointerdown
+        // Prevent context menu from appearing on pointerdown
         // on either touchOverlay or virtual controller
+        this.disableContextMenu();
+
+        // Initialize event listeners
+        this.addKeyboardListener();
+
+        // Event listeners for pointerdown and pointerup events
+        this.addPointerListener();
+
+    }
+
+    disableContextMenu() {
         document.addEventListener("contextmenu", (event) => {
-            event.preventDefault()
-        })
-
+            event.preventDefault();
+        });
         document.addEventListener("contextmenu", (event) => {
-            event.preventDefault()
-        })
-
-        // add event listerners for pointerdown and pointerup
-        document.addEventListener('pointerdown', (event) => {
-            event.preventDefault()
-            this.handlePointerEvent(event)
-        },)
-        document.addEventListener('pointerup', (event) => {
-            event.preventDefault()
-            this.handlePointerEvent(event)
-        })
-
-
+            event.preventDefault();
+        });
     }
 
-    processGamepadInput(gamepad) {
-        // Button example (A button)
-        if (gamepad.buttons[0].pressed) {
-            console.log("A button is pressed")
-        }
-        if (gamepad.buttons[1].pressed) {
-            console.log("B button is pressed")
-        }
-        if (gamepad.buttons[2].pressed) {
-            console.log("X button is pressed")
-        }
-        if (gamepad.buttons[3].pressed) {
-            console.log("Y button is pressed")
-        }
-
-        // Left stick - horizontal movement
-        let leftStickX = gamepad.axes[0]
-        if (leftStickX < -0.5) {
-            console.log("Left is pressed")
-        } else if (leftStickX > 0.5) {
-            console.log("Right is pressed")
-        }
 
 
-
-        // More buttons and axes processing...
+    // Add event listeners for keyboard input
+    addKeyboardListener() {
+        // Event listener for keyboard input
+        addEventListener("keydown", (e) => {
+            e.preventDefault();
+            this.keyHandler(e);
+        });
+        addEventListener("keyup", (e) => this.keyHandler(e));
     }
 
-    initTouches() {
 
+    // Key handler to convert keyboard input into input handler action verbs
+    keyHandler(e) {
+        let isKeyPressed = e.type == "keydown" ? true : false;
 
-        let uiElements = Object.values(this.ui.elements)
-
-        uiElements.forEach((element) => {
-            // console.dir(element)
-            if (element.dataset.events) {
-                // console.log("hasevents")
-                let eventsList = element.dataset.events.split(" ")
-                // console.dir(eventsList)
-                // console.dir(element)
-                eventsList.forEach((eventType) => {
-                    // console.dir(element)
-                    // console.log(eventType)
-                    switch (eventType) {
-                        case "touchstart":
-                            element.addEventListener("touchstart", (e) => {
-                                this.listenForTouches(e)
-                                console.log("touchstart")
-                            })
-                            break
-                        case "touchend":
-                            element.addEventListener("touchend", (e) => {
-                                this.listenForTouches(e)
-                                console.log("touchend")
-                            })
-                            break
-                        case "pointerdown":
-                            element.addEventListener("pointerdown", (e) => {
-                                // e.preventDefault()
-                                this.handlePointerEvent(e, element)
-                                console.log("pointerdown")
-                            })
-                            break
-                        case "pointerup":
-                            element.addEventListener("pointerup", (e) => {
-                                // e.preventDefault()
-                                this.handlePointerEvent(e, element)
-                                console.log("pointerup")
-                            })
-                            break
-
-                        default:
-                            console.log("No event listener added.")
-
-                    }
-                })
-
-            }
-        })
-
-    }
-
-    listenForKeys(e) {
-        let isKeyPressed = e.type == "keydown" ? true : false
-
-
+        console.log(e.key);
         // const previousKeyPressed = isKeyPressed
 
         switch (e.key) {
             case "ArrowRight":
             case "d":
             case "D":
-                this.right = isKeyPressed
+                this.right = isKeyPressed;
                 // console.log("🚀 ~ InputHandler ~ listenForKeys ~ this.right:", this.right)
-                break
+                break;
             case "ArrowLeft":
             case "a":
             case "A":
-                this.left = isKeyPressed
+                this.left = isKeyPressed;
                 // console.log("🚀 ~ InputHandler ~ listenForKeys ~ this.left:", this.left)
                 // // console.log("🚀 ~ InputHandler ~ listenForKeys ~ left:", this.left)
-                break
+                break;
             case "ArrowUp":
             case "w":
             case "W":
             case " ":
-                this.up = isKeyPressed
+                this.up = isKeyPressed;
                 // console.log("🚀 ~ InputHandler ~ listenForKeys ~ this.up:", this.up)
                 // // console.log("🚀 ~ InputHandler ~ listenForKeys ~ this.up:", this.up)
-                break
+                break;
             case "ArrowDown":
             case "s":
             case "S":
-                this.down = isKeyPressed
+                this.down = isKeyPressed;
                 // console.log("🚀 ~ InputHandler ~ listenForKeys ~ this.down:", this.down)
                 // // console.log("🚀 ~ InputHandler ~ listenForKeys ~ this.down:", this.down)
-                break
+                break;
             case "Escape":
-                this.escape = isKeyPressed
-                break
+                this.escape = isKeyPressed;
+                break;
             default:
-                break
+                break;
         }
     }
 
-    handlePointerEvent(event) {
-        let targetElement = event.target
+    addPointerListener() {
+        document.addEventListener('pointerdown', (event) => {
+            event.preventDefault();
+            this.pointerHandler(event);
+        });
+        document.addEventListener('pointerup', (event) => {
+            event.preventDefault();
+            this.pointerHandler(event);
+        });
+    }
+
+    // Pointer handler to convert pointer events to input handler action
+    pointerHandler(event) {
+        let targetElement = event.target;
 
         if (targetElement.id === "touchcontroller_right" ||
             targetElement.id === "touchcontroller_left" ||
@@ -183,42 +118,63 @@ export default class InputHandler {
             targetElement.id === "virtual-controller--button-select" ||
             targetElement.id === "virtual-controller--button-start") {
 
-            // console.log(targetElement)
-            let isButtonPressed = event.type === "pointerdown" ? true : false
-            // console.log(isButtonPressed)
-
-            // console.log(isButtonPressed)
+            let isButtonPressed = event.type === "pointerdown" ? true : false;
 
             switch (targetElement.id) {
                 case "touchcontroller_left":
                 case "virtual-controller--button-dpad-left":
                     // console.log
-                    this.left = isButtonPressed
+                    this.left = isButtonPressed;
                     // console.log("🚀 ~ InputHandler ~ listenForMouse ~ this.left:", this.left)
-                    break
+                    break;
                 case "touchcontroller_right":
                 case "virtual-controller--button-dpad-right":
-                    this.right = isButtonPressed
+                    this.right = isButtonPressed;
                     // console.log("🚀 ~ InputHandler ~ listenForMouse ~ this.right:", this.right)
-                    break
+                    break;
                 case "virtual-controller--button-primary":
-                    this.up = isButtonPressed
-                    break
+                    this.up = isButtonPressed;
+                    break;
                 case "virtual-controller--button-select":
-                    this.escape = isButtonPressed
-                    break
+                    this.escape = isButtonPressed;
+                    break;
                 case "virtual-controller--button-start":
-                    this.escape = isButtonPressed
-                    break
+                    this.escape = isButtonPressed;
+                    break;
                 default:
-                    break
+                    break;
 
             }
 
         }
-        return false
+        return false;
 
 
+    }
+
+    // TODO: Fix this
+    processGamepadInput(gamepad) {
+        // Button example (A button)
+        if (gamepad.buttons[0].pressed) {
+            console.log("A button is pressed");
+        }
+        if (gamepad.buttons[1].pressed) {
+            console.log("B button is pressed");
+        }
+        if (gamepad.buttons[2].pressed) {
+            console.log("X button is pressed");
+        }
+        if (gamepad.buttons[3].pressed) {
+            console.log("Y button is pressed");
+        }
+
+        // Left stick - horizontal movement
+        let leftStickX = gamepad.axes[0];
+        if (leftStickX < -0.5) {
+            console.log("Left is pressed");
+        } else if (leftStickX > 0.5) {
+            console.log("Right is pressed");
+        }
     }
 
 }
